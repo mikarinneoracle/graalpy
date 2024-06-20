ceaa3aaad276b3ea01bf542cb51902155ddbf744
Mika Rinne <mika.rinne@oracle.com>
2024-06-20 15:40:37 +0300

    testing the binary

--- src/func.py
+++ src/func.py
@@ -1,21 +1,24 @@
-import io
-import json
-import logging
+print('Hello, world!')
 
-from fdk import response
+#import io
+#import json
+#import logging
+
+#from fdk import response
 
 
-def handler(ctx, data: io.BytesIO = None):
-    name = "World"
-    try:
-        body = json.loads(data.getvalue())
-        name = body.get("name")
-    except (Exception, ValueError) as ex:
-        logging.getLogger().info('error parsing json payload: ' + str(ex))
+#def handler(ctx, data: io.BytesIO = None):
+#    name = "World"
+#    try:
+#        body = json.loads(data.getvalue())
+#        name = body.get("name")
+#    except (Exception, ValueError) as ex:
+#        logging.getLogger().info('error parsing json payload: ' + str(ex))
 
-    logging.getLogger().info("Inside Python Hello World function")
-    return response.Response(
-        ctx, response_data=json.dumps(
-            {"message": "Hello {0}".format(name)}),
-        headers={"Content-Type": "application/json"}
-    )
+#    logging.getLogger().info("Inside Python Hello World function")
+#    return response.Response(
+#        ctx, response_data=json.dumps(
+#            {"message": "Hello {0}".format(name)}),
+#        headers={"Content-Type": "application/json"}
+#    )
+s
